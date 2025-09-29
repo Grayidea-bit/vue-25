ae3aa801490fb72d840b7acc0aa1db387c755417
Grayidea-bit <lurenyi246891@gmail.com>
2025-09-29 15:30:01 +0800

    rm: all file

--- app/app.vue
+++
@@ -1,9 +0,0 @@
-<template>
-  <div class="flex flex-col min-h-screen scroll-auto gap-12">
-  <AppHeader />
-    <NuxtLayout>
-      <NuxtPage />
-    </NuxtLayout>
-  <AppFooter />
-  </div>
-</template>
--- app/layouts/default.vue
+++
@@ -1,5 +0,0 @@
-<template>
-    <div class="p-8 max-w-6xl flex flex-col mx-auto">
-        <slot />
-    </div>
-</template>

5f9040678f14d83314058bd597ca843b44d7e22f
Grayidea-bit <lurenyi246891@gmail.com>
2025-10-01 14:52:29 +0800

    feat: hover & test pic in card

--- src/components/common/Card/Card.vue
+++ src/components/common/Card/Card.vue
@@ -1,18 +1,27 @@
 <script setup lang="ts">
-const props = defineProps<{
-    title: string;
-    description: string;
-    // miniature: string;
-}>();
+
+    const props = withDefaults(defineProps<{
+        title: string;
+        description: string;
+        miniature: string;
+        picLoc?: 'right' | 'left';
+    }>(), {
+        picLoc: 'left'
+    });
+
+    const picLoc = props.picLoc === 'right' ? 'flex-row-reverse' : 'flex-row';
+
 
 </script>
 
 
 <template>
-    <div class="w-full h-40 flex flex-row border-2 border-gray-300 rounded-lg overflow-hidden relative">
-        <!-- <img :src="props.miniature" alt="Miniature" class="absolute inset-0 w-full h-full object-cover" /> -->
+    <div :class="['w-full h-60 flex border-2 border-gray-300 rounded-lg overflow-hidden relative hover:scale-110 transition-transform duration-300 ease-in-out', picLoc]">
+        <div class="w-3/5 h-full bg-gray-800 flex flex-col items-center overflow-hidden">
+            <img :src="props.miniature" alt="Miniature" class="h-full inset-0 object-cover" />
+        </div>
         <div 
-            class="w-full h-full bg-cover bg-center flex flex-col justify-center items-center"
+            class="w-full h-full flex flex-col items-left p-4 gap-4"
         >
             <h1 class="text-6xl font-bold">{{ props.title }}</h1>
             <p class="text-lg">{{ props.description }}</p>
--- src/pages/Portfolio.vue
+++ src/pages/Portfolio.vue
@@ -10,10 +10,10 @@ import {Card, cardLayout} from '@/components/common/Card';
         <div class="flex flex-col gap-4 items-center py-4">
             <h1 class="text-5xl font-bold">Portfolio Page</h1>
             <cardLayout>
-                <Card title="Project 1" description="Description of Project 1"  />
-                <Card title="Project 2" description="Description of Project 2"  />
-                <Card title="Project 3" description="Description of Project 3"  />
-                <Card title="Project 4" description="Description of Project 4"  />
+                <Card title="Project 1" description="Description of Project 1" miniature="/testPic1.jpeg" picLoc="right"/>
+                <Card title="Project 2" description="Description of Project 2" miniature="/herobg.jpg" picLoc="left"/>
+                <Card title="Project 3" description="Description of Project 3" miniature="/testPic2.webp" picLoc="right"/>
+                <Card title="Project 4" description="Description of Project 4" miniature="/herobg.jpg" />
             </cardLayout>
         </div>
     </ContentLayout>
